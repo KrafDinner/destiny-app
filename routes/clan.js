f13a5975f2bffe1e94a626118523fbd6134a807f
Cody Knapp <krafdinner@gmail.com>
2017-11-06 22:55:08 -0500

    Adding a sample rest call to the clan endpoint

--- routes/clan.js
+++ routes/clan.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request-promise-native');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    let options = {
+        method: 'GET',
+        headers: {
+            "X-API-Key": "30052613c3424fb8a1a55fef5c916910"
+        },
+        uri: 'https://www.bungie.net/Platform/GroupV2/Name/Fireteam Herp Derp/1/',
+    };
+
+    let result;
+    request(options).then((body) => {
+        res.send(body);
+    });
 });
 
 module.exports = router;
